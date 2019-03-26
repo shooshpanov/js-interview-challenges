3d1dff8ec098874f1e30821b0cd627d555a3d413
Sergey S <shooshpanov@yandex.com>
2019-03-26 12:17:07 -0400

    first commit

---
+++ palindorme.js
@@ -0,0 +1,13 @@
+const palindrome = str => {
+  // turn the string to lowercase
+  str = str.toLowerCase();
+  // reverse input string and return the result of the
+  // comparisong
+  return (
+    str ===
+    str
+      .split("")
+      .reverse()
+      .join("")
+  );
+};
